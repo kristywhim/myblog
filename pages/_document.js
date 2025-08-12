a8a9f7e067ffa507725c01fdf24f08d41ed01128
Krypton M. <86606147+kristywhim@users.noreply.github.com>
2025-08-11 22:54:56 -0700

    update Gtag

--- pages/_document.js
+++ pages/_document.js
@@ -68,6 +68,21 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+            <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_GOOGLE_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+          >
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_GOOGLE_ID}');
+            `}
+          </Script>
         </body>
       </Html>
     )
